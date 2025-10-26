d169ca2ba816cb4f2a517c43b5b0140331b2a6f0
GersonResplandes <sagerson@acad.ifma.edu.br>
2025-10-26 16:38:27 -0300

    refactor: migra servidor http para Express.js

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-// 1. Importar o módulo 'http'
-const http = require("http");
-
-// 2. Definir a porta
+const express = require("express");
+const app = express();
 const port = 3000;
 
-// 3. Criar o servidor
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.end("Olá, Mundo");
-  } else if (req.url === "/sobre") {
-    res.end("Esta é a página sobre");
-  } else {
-    res.end("Página não encontrada");
-  }
+app.get("/", (req, res) => {
+  res.send("Olá, Mundo");
 });
 
-// 4. Iniciar o servidor e fazê-lo "ouvir" na porta
-server.listen(port, () => {
-  // (Adicionei este console.log como um bônus)
-  // É uma boa prática nos avisar no terminal que o servidor está no ar.
+app.get("/sobre", (req, res) => {
+  res.send("Esta é a página sobre");
+});
+
+app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
